762a72f73e6ab1ed14dc13f194739939b5c9fe55
Hongmin Chen <T-chm@users.noreply.github.com>
2024-07-28 23:53:47 -0400

    update phrase

--- config.browser.ts
+++ config.browser.ts
@@ -1,4 +1,4 @@
-import { samplePhrases } from "./prompts/movie-critic";
+import { samplePhrases } from "./prompts/epic";
 // import { samplePhrases } from "./prompts/tour-guide";
 
 export const appConfig = {
